1165d6fd51d013a12f1157bb915fb9b54f00312d
kkv <vinstark@finports.com>
2020-04-16 17:25:53 +0900

    type formating

--- README.md
+++ README.md
@@ -1,16 +1,19 @@
 # Python Study
 ### 파이썬 파일 경로는 /apache2/htdocs 입니다.
 
-## 1 ~ 12장
+## 1 ~ 12장 Setting
 #### python 세팅과 bitnami mampstack을 사용하여 웹과 연동
 
-## 13 장
-#### Python 데이터타입 - 숫자 -
+## 13장 Python 데이터타입 - 숫자 -
 #### NuberType 종류 : https://docs.python.org/3.7/library/stdtypes.html
 #### Int 타입의 기본 연산자
 
-## 14장
-#### Python 데이터타입 -문자열-
+## 14 ~ 16장 Python 데이터타입 - 문자열 -
 #### '로 시작하면 '로 끝나야함.
 #### \를 붙이면 문자열로 인식하게 됨. (escape)
 #### escape 목록 : http://python-ds.com/python-3-escape-sequences
+
+## 17장 Python 데이터타입 포맷팅
+#### Formating 종류 : https://pyformat.info/
+#### positional formating = 위치 기반 포맷팅
+#### named placeholder = 특정위치 기반 포맷팅
---
+++ apache2/htdocs/syntax/string4.py
@@ -0,0 +1,5 @@
+#positional formating
+print('to {}. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut apple labore et dolore magna aliqua. {} Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, {} sunt in culpa qui officia deserunt mollit anim id est laborum.'.format('terry', 12, 'terry', 13))
+
+#named placeholder
+print('to {name}. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut apple labore et dolore magna aliqua. {age} Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, {name} sunt in culpa qui officia deserunt mollit anim id est laborum.'.format(name='terry', age=12))
