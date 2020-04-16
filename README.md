9ace2aa06264cb985b4e1d09b64063b4966428b4
kkv <vinstark@finports.com>
2020-04-16 15:34:01 +0900

    python setting

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-test
+# Python Study
+### 파이썬 파일 경로는 /apache2/htdocs 입니다.
+
+## 1 ~ 13장
+#### python 세팅과 bitnami mampstack을 사용하여 웹과 연동
--- apache2/htdocs/helloworld.py
+++ apache2/htdocs/helloworld.py
@@ -1,3 +1,5 @@
+#!/Library/Frameworks/Python.framework/Versions/3.7/bin/python3
+print("content-type:text/html; charset=utf-8\n")
 a = 3+4+5
 b = a/3
 
